d4f9eea86baccaf3899e2f31a62ab39679a2158c
KMP2000 <108013872+KMP2000@users.noreply.github.com>
2023-07-06 16:38:40 +0530

    Update 19_sets_in_python.py

--- Week-1/19_sets_in_python.py
+++ Week-1/19_sets_in_python.py
@@ -1,9 +1,9 @@
-a = {1, 3, 4, 5}
-print(type(a))
-print(a)
+variable1 = {1, 3, 4, 5}
+print(type(variable1))
+print(variable1)
 
 mySet = {1,0,"sre",(1,2,3)}
 print(mySet)
 
-a  = {6,1,3,4,5}
-print(a)
+variable2  = {6,1,3,4,5}
+print(variable2)
