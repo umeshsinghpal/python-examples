a850631bec48542545bcf8b8c05467444929fce9
KMP2000 <108013872+KMP2000@users.noreply.github.com>
2023-07-06 16:32:28 +0530

    Update 09_string_slicing.py

--- Week-1/09_string_slicing.py
+++ Week-1/09_string_slicing.py
@@ -3,8 +3,8 @@
 # print(type(name))
 
 # Concatenating two strings
-# c = greeting + name
-# print(c)
+# concat= greeting + name
+# print(concat)
 name = "Krish"
 # print(name[4])
 # name[3] = "d" --> Does not work
@@ -12,8 +12,8 @@ name = "Krish"
 # print(name[1:4])
 # print(name[:4]) # is same as name[0:4]
 # print(name[1:]) # is same as name[1:5]
-# c = name[-4:-1] # is same is name[1:4]
-# print(c)
+# concat= name[-4:-1] # is same is name[1:4]
+# print(concat)
 
 name = "SreIsGood"
 print(name)
