18bef947ace92928a33fe308425eb00eafbdae12
chetanguptasre <110661710+chetanguptasre@users.noreply.github.com>
2023-07-25 11:54:21 +0530

    Update fileexample.py

--- Week-4/fileexample.py
+++ Week-4/fileexample.py
@@ -1,12 +1,22 @@
 import json
 
 
+def getting_age():
+    try:
+        return int(input("Please enter your age : "))
+    except:
+        return False
+
+
 people = {}
 id = 0
 while True:
     one_person = {}
     one_person['name'] = input("Please enter your name : ")
-    one_person['age'] = int(input("Please enter your age : "))
+
+    while getting_age() == False :
+        one_person['name'] = getting_age()
+
     one_person['country'] = input("Please enter your country : ")
     id += 1
     people[id] = one_person
