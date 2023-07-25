7b14f2cc5dfd8c0b9657cf3c6ce28f55cc599473
chetanguptasre <110661710+chetanguptasre@users.noreply.github.com>
2023-07-25 13:07:32 +0530

    Update fileexample.py

--- Week-4/fileexample.py
+++ Week-4/fileexample.py
@@ -1,12 +1,6 @@
+# final stage
+
 import json
-
-
-def getting_age():
-    try:
-        return int(input("Please enter your age : "))
-    except:
-        return False
-
 
 people = {}
 id = 0
@@ -14,17 +8,41 @@ while True:
     one_person = {}
     one_person['name'] = input("Please enter your name : ")
 
-    while getting_age() == False :
-        one_person['name'] = getting_age()
+    while True:
+        try:
+            one_person['age'] = int(input("Please enter your age : "))
+            break
+        except:
+            continue
 
     one_person['country'] = input("Please enter your country : ")
+
     id += 1
+
     people[id] = one_person
     if input("Do you want to continue? ").lower() != 'y':
         break
 
 print(f'You are going to be writing this\n\n{people.__str__()}')
 
-with open('./files/people.json', 'a') as filehandler:
+with open('./files/people.json', 'w') as filehandler:
 
     filehandler.write(json.dumps(people))
+
+
+
+
+
+# first stage
+
+# import json
+
+# with open("./sample-files/sample-json.json",'r') as jsonhandler:
+#     jsoncontent = json.loads(jsonhandler.read())
+
+#     print(jsoncontent["firstName"])
+
+#     for items in jsoncontent:
+#         print(items,jsoncontent[items])
+
+
